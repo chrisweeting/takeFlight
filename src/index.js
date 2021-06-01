15193bf0c4a7b3000f296e43e211255b798b1871
chrisweeting <chrisweeting@gmail.com>
2021-06-01 15:51:30 -0400

    Cactus movement

--- src/index.js
+++ src/index.js
@@ -4,35 +4,40 @@ var canvas = document.getElementById("flyCanvas");
 var ctx = canvas.getContext("2d");
 var rightPressed = false;
 var leftPressed = false;
-var yInitial = 0;
+var turnX = 0;
 var y = 0;
 var rotationInit = 0;
 var rotation = 0;
+var cacti = {
+  0: { x: -80, y: -60, width: 20, height: 60 },
+  1: { x: -30, y: -60, width: 20, height: 60 },
+  2: { x: 30, y: -60, width: 20, height: 60 },
+};
 
 ctx.translate(240, 200);
 
 
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
+const keyDownHandler = (e) => {
   if (e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = true;
   }
   else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = true;
   }
-}
+};
 
-function keyUpHandler(e) {
+const keyUpHandler = (e) => {
   if (e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = false;
   }
   else if (e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = false;
   }
-}
+};
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 const drawHorizon = () => {
   ctx.beginPath();
@@ -41,13 +46,13 @@ const drawHorizon = () => {
   ctx.fillStyle = "#e8e2a4";
   ctx.fill();
   ctx.closePath();
-}
+};
 
 const drawRotation = () => {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD";
   ctx.fillText("Rotation: " + rotation, 1, 1);
-}
+};
 
 const rotateCam = (num) => {
   if (num < 100 && num > 0) {
@@ -55,30 +60,50 @@ const rotateCam = (num) => {
   } else if (num > -100 && num < 0) {
     return ctx.rotate((Math.PI / 180) * -0.05);
   }
-}
+};
 
 const drawBird = () => {
   ctx.save();
-
-  ctx.translate(0, 70);
-  ctx.rotate((Math.PI / 180) * -(rotation / 10));
+  
+  ctx.translate(0, 50);
+  ctx.rotate((Math.PI / 180) * -(rotation / 20));
   ctx.beginPath();
   ctx.rect(-30, -10, 60, 20);
   ctx.fillStyle = "blue";
   ctx.fill();
   ctx.closePath();
-
+  
   ctx.restore();
-}
+};
+
+const drawCactus = (x, y, w, h) => {
+  // const place = -30 + x;
+  // const width = 20 + w;
+  // const height = 60 + h;
+  // const growingX = place + gX;
+  // const growingY =
+  ctx.beginPath();
+  ctx.rect(x, y, w, h);
+  ctx.fillStyle = "#a1d6a2";
+  ctx.fill();
+  ctx.closePath();
+};
 
 const animate = () => {
   ctx.clearRect(-1000, -1000, 2000, 2000);
   drawHorizon();
   drawRotation();
   drawBird();
+  for (const i in cacti) {
+    let cactus = cacti[i];
+    cactus.x += turnX;
+    drawCactus(cactus.x, cactus.y, cactus.width, cactus.height);
+  }
+  // turnX = 0;
 
   // ctx.rotate((Math.PI / 180) * rotation);
   if (leftPressed) {
+    turnX += 0.01;
     if (rotation < 100 && rotation >= 0) {
       rotation += 1;
       rotateCam(rotation);
@@ -87,6 +112,7 @@ const animate = () => {
       ctx.rotate((Math.PI / 180) * 0.05);
     }
   } else if (rightPressed) {
+    turnX -= 0.01;
     if (rotation > -100 && rotation <= 0) {
       rotation -= 1;
       rotateCam(rotation);
@@ -101,10 +127,11 @@ const animate = () => {
     } else if (rotation < 0) {
       rotation += 1;
       ctx.rotate((Math.PI / 180) * 0.05);
+    } else {
+      turnX = 0;
     }
   }
-}
-
+};
 
 
 setInterval(animate, 10);
